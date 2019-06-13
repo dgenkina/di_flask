f1401cbfceaad9c67b9d708a4b6a86bc5ee95e6f
Dina Genkina <dgenkina@umd.edu>
2019-06-13 16:34:21 -0400

    First pass at getting the form filling to work

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
+
+from wtforms import Form
+from wtforms import fields, validators
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
 
 app = Flask(__name__)
 
+class SetDataParamsForm(Form):
+    ticker = fields.StringField()
+    year = fields.IntegerField('Year', [validators.NumberRange(min=2005, max=2019)])
+    month = fields.IntegerField('Month', [validators.NumberRange(min=1, max=12)])
+
+  #  site = QuerySelectField(query_factory=Site.query.all)
+
 @app.route('/')
 def index():
-  return render_template('index.html')
+    set_data_params_form = SetDataParamsForm()
+    return render_template('index.html', set_data_params_form=set_data_params_form)
 
 @app.route('/about')
 def about():
-  return render_template('about.html')
+    return render_template('about.html')
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    app.run(port=33507)
