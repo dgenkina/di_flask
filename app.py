8cdaf9f1e2c8c937f69facb524830ca5dd3982e8
Dina Genkina <dinkazavr@gmail.com>
2019-06-18 16:02:14 -0400

    Working version, need to figure out how to make the year and months selection drop downs for year/month that has data available for that ticker.

--- app.py
+++ app.py
@@ -3,24 +3,49 @@ from flask import Flask, render_template, request
 from wtforms import Form
 from wtforms import fields, validators
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
+import pandas as pd
+import re
 
+from get_data_and_plot import plot_closing_prices
 app = Flask(__name__)
 
+
+ticker_list = pd.read_csv('WIKI_metadata.csv')['code'].tolist()
+stock_list = pd.read_csv('WIKI_metadata.csv')['name'].tolist()
+tick_list = []
+for ind,tick in enumerate(ticker_list):
+    name = re.split('\(',stock_list[ind])[0]
+    tick_list.append((tick,name))
+    
 class SetDataParamsForm(Form):
-    ticker = fields.StringField()
+    ticker = fields.SelectField('ticker', choices = tick_list)
     year = fields.IntegerField('Year', [validators.NumberRange(min=2005, max=2019)])
     month = fields.IntegerField('Month', [validators.NumberRange(min=1, max=12)])
 
   #  site = QuerySelectField(query_factory=Site.query.all)
 
-@app.route('/')
-def index():
+@app.route('/tickers', methods=['GET', 'POST'])
+def tickers():
     set_data_params_form = SetDataParamsForm()
-    return render_template('index.html', set_data_params_form=set_data_params_form)
+    if request.method == 'GET':
+        plot = None
+    elif request.method == 'POST':
+        ticker = str(request.form.get('ticker'))
+        year = int(request.form.get('year'))
+        month = int(request.form.get('month'))
+        plot = plot_closing_prices(ticker_name=ticker,year=year,month=month)
+
+    return render_template('tickers.html', form=set_data_params_form, plot=plot)
+
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(port=33507)
--- get_data_and_plot.py
+++ get_data_and_plot.py
@@ -7,30 +7,42 @@ Created on Fri Jun  7 11:58:08 2019
 
 import pandas as pd
 from bokeh.plotting import figure, output_file, show
+from bokeh.resources import CDN
+from bokeh.embed import file_html
 
-
-ticker_name = 'ZUMZ' #input('Ticker name: ')
-year = 2005
-month = 6
-
-api_key = "GFFgKCaH23x6cteSxCsx"
-request = "https://www.quandl.com/api/v3/datatables/WIKI/PRICES.csv?ticker=%s&api_key=%s" %(ticker_name,api_key)
-
-data = pd.read_csv(request)
-data['date'] = pd.to_datetime(data['date'])
-#quandl.ApiConfig.api_key = "GFFgKCaH23x6cteSxCsx"
-#data = quandl.get_table('WIKI/PRICES', ticker = ticker_name,paginate=True)
-
-if data.shape[0]==0:
-    print('This ticker is not in the database. Please try again')
-    raise SystemExit(0)
+def plot_closing_prices(ticker_name = 'ZUMZ',year=2005,month=6):
+    api_key = "GFFgKCaH23x6cteSxCsx"
+    request = "https://www.quandl.com/api/v3/datatables/WIKI/PRICES.csv?ticker=%s&api_key=%s" %(ticker_name,api_key)
     
-data = data.loc[data['date'].dt.year == year]
-data = data.loc[data['date'].dt.month == month]
-
-output_file("%s_close_value_from_%i_%i.html" %(ticker_name,month,year))
-
-p = figure(title="Closing stock prices", x_axis_label='date in %i' %(year), y_axis_label='price',  x_axis_type="datetime")
-p.line(data['date'].tolist(), data['close'].tolist())
-p.circle(data['date'].tolist(), data['close'].tolist(), fill_color="white", size=8)
-show(p)
+    data = pd.read_csv(request)
+    data['date'] = pd.to_datetime(data['date'])
+    #quandl.ApiConfig.api_key = "GFFgKCaH23x6cteSxCsx"
+    #data = quandl.get_table('WIKI/PRICES', ticker = ticker_name,paginate=True)
+    
+    if data.shape[0]==0:
+        html = '%s is not in the database. Please try again' %(ticker_name)
+        return html
+  
+    else:
+        
+        data = data.loc[data['date'].dt.year == year]
+        if data.shape[0]==0:
+            html = 'The year %i is not in the database for %s. Please try again' %(year,ticker_name)
+            return html
+        else:
+            if data.shape[0]==0:
+                html = 'The month %i is not in the database for %s in %i. Please try again' %(month,ticker_name,year)
+                return html
+            else:
+                data = data.loc[data['date'].dt.month == month]
+                
+                #output_file("%s_close_value_from_%i_%i.html" %(ticker_name,month,year))
+                
+                p = figure(title="Closing stock prices for %s" %(ticker_name), x_axis_label='date in %i' %(year),
+                           y_axis_label='price',  x_axis_type="datetime",plot_width=800, plot_height=300
+                           )
+                p.line(data['date'].tolist(), data['close'].tolist())
+                p.circle(data['date'].tolist(), data['close'].tolist(), fill_color="white", size=8)
+                #show(p)
+                html = file_html(p, CDN, p.title)
+                return html
